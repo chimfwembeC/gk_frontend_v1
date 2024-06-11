27510f9d0a4dbf9d9d5191aa1db2cc799f445c0c
chimfwembeC <chimfwembek@kamstar.tech>
2024-06-11 03:03:16 +0200

    speed insights

--- src/components/AppNavbar.js
+++ src/components/AppNavbar.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 const AppNavbar = () => {
   return (
+    <>
     <Navbar color="dark" dark expand="md">
       <NavbarBrand href="/">GK Renovations</NavbarBrand>
       <Nav className="ml-auto" navbar>
@@ -11,6 +12,8 @@ const AppNavbar = () => {
         </NavItem>
       </Nav>
     </Navbar>
+    <SpeedInsights />
+    </>
   );
 };
 
